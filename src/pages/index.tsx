3680cdf0dc6376d05235c1d665cea0f3617b98d4
Misiorny <krzystan@wp.pl>
2023-01-09 12:23:13 +0100

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+  return (
+    <div className='flex  bg-amber-300 shadow-xl drop-shadow-2xl shadow-blue-700 mx-auto max-w-md mt-3'>
+      <p className='mx-auto px-4 py-2 mt-3 text-3xl '>Tu bedzie kiedyś nowa strona </p>
+    </div>
+  )
+}
+
+export default Home
